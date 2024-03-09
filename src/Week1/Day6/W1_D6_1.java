84d23728cd3250fb344686f9b47d6f78be90b414
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-09 23:23:42 +0900

    Milla Week1 Day6 Challenge edit

--- src/Week1/Day6/W1_D6_1.java
+++ src/Week1/Day6/W1_D6_1.java
@@ -1,4 +1,18 @@
 package Week1.Day6;
 
+import java.util.*;
+
 public class W1_D6_1 {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+
+        for(int i=0;i<prices.length;i++){
+            for(int j=i+1;j<prices.length;j++){
+                answer[i]++;
+                if (prices[i] > prices[j]) break;
+            }
+        }
+
+        return answer;
+    }
 }
