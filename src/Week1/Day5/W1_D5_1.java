2c8391bbd0210e29e2a59ca231b564b1504db077
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-09 23:11:16 +0900

    Milla Week1 Day5 Challenge edit

--- src/Week1/Day5/W1_D5_1.java
+++ src/Week1/Day5/W1_D5_1.java
@@ -1,7 +1,53 @@
 package Week1.Day5;
 
+import java.util.*;
+
 public class W1_D5_1 {
+    static class Document {
+        int priority;
+        int location;
+
+        public Document(int priority, int location) {
+            this.priority = priority;
+            this.location = location;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            int count = 0;
+            int n = in.nextInt();
+            int m = in.nextInt();
+            in.nextLine();
+
+            String input = in.nextLine();
+            String[] splitInput = input.split(" ");
+            Queue<Document> queue = new LinkedList<>();
+            PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Comparator.reverseOrder());
+
+            for (int j = 0; j < splitInput.length; j++) {
+                int priority = Integer.parseInt(splitInput[j]);
+                queue.add(new Document(priority, j));
+                priorityQueue.add(priority);
+            }
+
+            while (!queue.isEmpty()) {
+                Document current = queue.poll();
+                if (current.priority == priorityQueue.peek()) {
+                    count++;
+                    priorityQueue.poll();
+                    if (current.location == m) {
+                        break;
+                    }
+                } else {
+                    queue.add(current);
+                }
+            }
+
+            System.out.println(count);
+        }
     }
 }
--- src/Week1/Day5/W1_D5_2.java
+++ src/Week1/Day5/W1_D5_2.java
@@ -1,7 +1,34 @@
 package Week1.Day5;
 
+import java.util.*;
+
 public class W1_D5_2 {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // TODO Auto-generated method stub
+
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < testCase; i++) {
+            Stack stack = new Stack<>();
+            String s = sc.nextLine();
+            for (int j = 0; j < s.length(); j++) {
+                if (s.charAt(j) == ' ') {
+                    while (!stack.empty()) {
+                        System.out.print(stack.pop());
+                    }
+                    System.out.print(" ");
+                } else {
+                    stack.add(s.charAt(j));
+                }
+            }
+            while(!stack.empty()) {
+                System.out.print(stack.pop());
+            }
+            System.out.println();
+
+        }
     }
 }
