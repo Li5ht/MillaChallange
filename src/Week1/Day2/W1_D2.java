a9ab7b2c8de3adfb416de502d5edb7fcac9e0be1
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-09 19:44:53 +0900

    Milla Week1 Day2 Challenge edit

--- src/Week1/Day2/W1_D2.java
+++ src/Week1/Day2/W1_D2.java
@@ -1,7 +1,34 @@
 package Week1.Day2;
 
+import java.util.*;
+
 public class W1_D2 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+       LinkedList<Integer> q = new LinkedList<>();
+
+       int n = sc.nextInt();
+       int k = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        int index = 0;
+
+        System.out.print("<");
+        while (!q.isEmpty()) {
+            index = (index + (k - 1)) % q.size();
+            if(q.size() != 1) {
+                System.out.print(q.remove(index) + ", ");
+            }else{
+                System.out.print(q.remove(index));
+            }
+        }
+
+        System.out.print(">");
+
+
+       sc.close();
     }
 }
