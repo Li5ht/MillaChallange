c6de7efa7b186a02197bcbfec394bc6ecc3f6b41
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-16 22:29:01 +0900

    Milla Week2 Day1 Challenge edit

--- src/Week2/Day1/W2_D1.java
+++ src/Week2/Day1/W2_D1.java
@@ -1,7 +1,35 @@
 package Week2.Day1;
 
+import java.util.*;
+import java.io.*;
+
 public class W2_D1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(in.readLine());
+
+        int K = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] lines = new int[K];
+        for (int i = 0; i < K; i++) lines[i] = Integer.parseInt(in.readLine());
+
+        Arrays.sort(lines);
+
+        long right = lines[K-1];
+        long left = 1;
+        long count, half;
+
+        while(left<=right) {
+            count = 0;
+            half=(left+right)/2;
+
+            for(int i=0;i<K;i++) count += lines[i]/half;
+
+            if(count<N) right = half-1;
+            else left = half+1;
+        }
+
+        System.out.println(right);
     }
 }
