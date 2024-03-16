b4e8c9ade4b063b4c1449b15ea1b2bb7c99288d5
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-16 23:04:35 +0900

    Milla Week2 Day5 Challenge edit

--- src/Week2/Day5/W2_D5.java
+++ src/Week2/Day5/W2_D5.java
@@ -1,8 +1,34 @@
 package Week2.Day5;
 
+import java.util.*;
+import java.io.*;
+
 public class W2_D5 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        HashMap<Integer, Integer> card = new HashMap<>();
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int inputNum = Integer.parseInt(st.nextToken());
+            if (card.containsKey(inputNum)) {
+                int count = card.get(inputNum);
+                card.put(inputNum, count + 1);
+            } else {
+                card.put(inputNum, 1);
+            }
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < M; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if (card.containsKey(num)) sb.append(card.get(num)).append(" ");
+            else sb.append(0).append(" ");
+        }
     }
 }
-
