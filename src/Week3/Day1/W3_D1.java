69dfe7491b9ec97c01786ca8d553d02d2002cb5b
Yeji Park <89853141+Li5ht@users.noreply.github.com>
2024-03-18 23:16:36 +0900

    Milla Week2 Day1 Challenge edit

--- src/Week3/Day1/W3_D1.java
+++ src/Week3/Day1/W3_D1.java
@@ -1,7 +1,14 @@
 package Week3.Day1;
 
+import java.util.*;
+import java.io.*;
+
 public class W3_D1 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] graph;
+        LinkedList<Integer> dfs = new LinkedList<>();
+        Queue<Integer> bfs = new LinkedList<>();
 
     }
 }
